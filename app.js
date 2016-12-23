b14658fbe0ba458fa59280fdc9a0fe0c896c2134
dpilipenko <dmitriy.pilipenko@gmail.com>
2016-12-22 23:06:18 -0500

    Break out business logic into steps

--- app.js
+++ app.js
@@ -8,10 +8,44 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (res, req) {
   return res.render('index');
 });
-
+/* validate file was submitted */
 app.post('/upload', upload.single('file'), function (req, res, next) {
-  console.log('image uploaded: ', req.file);
+  if (req.file) {
+    next();
+  } else {
+    res.status(400).send('no file was submitted');
+  }
+});
 
+/* validate file is an image */
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  if ((req.file.mimetype.indexOf('image/jpeg') > -1) || (req.file.mimetype.indexOf('image/gif') > -1) || (req.file.mimetype.indexOf('image/png') > -1)) {
+    next();
+  } else {
+    res.status(400).send('submitted file not an image');
+  }
+});
+
+/* validate file fits size criteria */
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  console.log('TODO: implement size validation');
+  next();
+});
+
+/* transform image and calculate coordinates of faces */
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  console.log('TODO: implement face detection and append calculated values to req for next middleware');
+  next();
+});
+
+/* transform image and draw googly-eyes image at coordinates of eyes */
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  console.log('TODO: implement image manipulation and append updated image to req for next middleware');
+  next();
+});
+
+/* respond with new image */
+app.post('/upload', upload.single('file'), function (req, res, next) {
   var options = {
     root: __dirname + '/uploads/',
     dotfiles: 'deny',
@@ -24,14 +58,12 @@ app.post('/upload', upload.single('file'), function (req, res, next) {
   res.type(req.file.mimetype);
   res.sendFile(req.file.filename, options, function (err) {
     if (err) {
-      console.log('err: ', err);
       res.status(err.status).end();
     } else {
-      console.log('sent: ', req.file.filename);
+      /* done */
     }
   });
 });
 
 app.listen(port);
 console.log('running on http://localhost:' + port);
-
